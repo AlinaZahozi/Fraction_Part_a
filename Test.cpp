6c2904ebe32db09b6fbf6f66160265191be0643e
AlinaZ <alinush0zahozi@gmail.com>
2023-04-23 19:02:58 +0300

    Update

---
+++ Test.cpp
@@ -0,0 +1,93 @@
+#include "doctest.h"
+#include <stdexcept>
+using namespace std;
+
+#include "sources/Fraction.hpp"
+using namespace ariel;
+
+
+TEST_CASE("Test 1-Test ") {
+    //CHECK( == );
+    //CHECK_THROWS();
+    //CHECK_NOTHROW();
+}
+
+
+TEST_CASE("Test 2-Test ") {
+
+}
+
+
+TEST_CASE("Test 3-Test") {
+
+}
+
+TEST_CASE("Test 4-Test") {
+
+}
+
+TEST_CASE("Test 5-Test") {
+
+}
+
+TEST_CASE("Test 6-Test") {
+
+}
+
+TEST_CASE("Test 7-Test") {
+
+}
+
+
+TEST_CASE("Test 8-Test") {
+
+}
+
+
+TEST_CASE("Test 9-Test") {
+
+}
+
+TEST_CASE("Test 10-Test") {
+
+}
+
+TEST_CASE("Test 11-Test") {
+    
+}
+
+TEST_CASE("Test 12-Test") {
+
+}
+
+TEST_CASE("Test 13-Test") {
+
+}
+
+TEST_CASE("Test 14-Test") {
+
+}
+
+TEST_CASE("Test 15-Test") {
+
+}
+
+TEST_CASE("Test 16-Test") {
+
+}
+
+TEST_CASE("Test 17-Test") {
+
+}
+
+TEST_CASE("Test 18-Test") {
+
+}
+
+TEST_CASE("Test 19-Test") {
+
+}
+
+TEST_CASE("Test 20-Test") {
+
+}
